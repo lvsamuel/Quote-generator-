ae068f8aeac1b9fbf59639c399c3c1d4ad4b02d6
lvsam <lucillevsam@gmail.com>
2017-08-27 01:17:02 +0000

    save data on submit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,6 +3,18 @@ class QuotesController < ApplicationController
         @quote= Quote.order("random()").first
     end
     def new
-        @quote =Quote.new
+         @quote = Quote.new
     end
+    
+     def create
+    Quote.create(quote_params)
+    redirect_to root_path
+     end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+    
 end
