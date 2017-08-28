dbfba4550da4cf2bdd2fa26a63a8eb990223c087
lvsam <lucillevsam@gmail.com>
2017-08-28 02:08:00 +0000

    adding modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,22 +2,22 @@ class QuotesController < ApplicationController
     def index
         @quote= Quote.order("random()").first
     end
-    def new
-         @quote = Quote.new
-    end
     
-     def create
-    Quote.create(quote_params)
+    def create
+    @quote = Quote.create(quote_params)
     if @quote.invalid?
-        flash[:error] = '<strong> could not save </strong> the data you entered is invalid.'
-    end   
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
     redirect_to root_path
-     end
+    end
+
+  def about
+  end
 
   private
 
-  def quote_params
+   def quote_params
     params.require(:quote).permit(:saying, :author)
-  end
+   end
     
 end
